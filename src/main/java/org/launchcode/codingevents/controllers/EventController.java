699399e64b088287b05892ac26a3f943e4553c5d
Chance Goodwin <cgood1413@gmail.com>
2022-02-17 16:46:24 -0600

    Walkthrough Updates

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("events")
@@ -25,8 +22,7 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String name, @RequestParam String address, @RequestParam String description){
-        Event newEvent = new Event(name, description, address);
+    public String createEvent(@ModelAttribute Event newEvent){
         EventData.addEvent(newEvent);
         return "redirect:";
     }
